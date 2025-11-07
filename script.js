4c2acddaa154d1bc7d4694d8974061478ca98739
Dave Okit <fernandookitakula@gmail.com>
2025-11-07 17:14:31 +0100

    pop-up

--- script.js
+++ script.js
@@ -3,17 +3,30 @@
 })();
 
 const form = document.getElementById("contact-form");
+const popUp = document.querySelector("#container-pop-up .pop-up");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   emailjs.sendForm("gmail_fernando", "template_u2uxucn", this).then(
-    () => {
-      alert("Mensagem enviada");
-      form.reset();
+    (data) => {
+      if (data) {
+        showpopUp()
+        form.reset();
+      } else {
+       
+      }
     },
     (error) => {
       alert("Erro ao enviar mensagem" + error.text);
     }
   );
 });
+
+function showpopUp() {
+    popUp.classList.add('active');
+
+    setInterval(() => {
+        popUp.classList.remove('active')
+    }, 3000);
+}
