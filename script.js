ea60326616dabe04ee393af519602f884cd610ba
Dave Okit <fernandookitakula@gmail.com>
2025-11-08 13:30:15 +0100

    Uptade of the day

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-(function () {
-  emailjs.init("Ve2GGgBVkx-z06xIc");
+const form = document.getElementById("contact-form");
+const successPopup = document.querySelector(".pop-up:not(.error)");
+const errorPopup = document.querySelector(".pop-up.error");
+
+(function() {
+  emailjs.init({
+    publicKey: "Ve2GGgBVkx-z06xIc", 
+  });
 })();
 
-const form = document.getElementById("contact-form");
-const popUp = document.querySelector("#container-pop-up .pop-up");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  emailjs.sendForm("gmail_fernando", "template_u2uxucn", this).then(
-    (data) => {
-      if (data) {
-        showpopUp()
-        form.reset();
-      } else {
-       
-      }
-    },
-    (error) => {
-      alert("Erro ao enviar mensagem" + error.text);
-    }
-  );
+emailjs.sendForm("gmail_fernando", "template_u2uxucn", this).then(
+  () => {
+    showPopup(successPopup);
+    form.reset();
+  },
+  (error) => {
+    showPopup(errorPopup);
+    console.error("Erro ao enviar o email:", error);
+  }
+);
 });
 
-function showpopUp() {
-    popUp.classList.add('active');
-
-    setInterval(() => {
-        popUp.classList.remove('active')
-    }, 3000);
+function showPopup(popup) {
+  popup.classList.add("active");
+  setTimeout(() => popup.classList.remove("active"), 3000);
 }
